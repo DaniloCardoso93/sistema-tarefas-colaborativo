c035923e04dd0979e6a3ce2ce9c11a8ad6981787
DaniloCardoso93 <levitano@hotmail.com>
2025-10-15 14:36:40 -0300

    feat(auth-service): implement user creation logic and add env config

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'challenge_db',
-      entities: [User],
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/auth-service/.env',
     }),
-    TypeOrmModule.forFeature([User]),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: 5432,
+        username: 'postgres',
+        password: 'password',
+        database: 'challenge_db',
+        entities: [User],
+        synchronize: false,
+      }),
+    }),
+
+    UsersModule,
   ],
   controllers: [],
   providers: [],
