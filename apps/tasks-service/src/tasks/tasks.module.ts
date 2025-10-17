006ca9e6ea78b2bd5b6c3c68d9e8ed5788519a7c
DaniloCardoso93 <levitano@hotmail.com>
2025-10-17 14:25:06 -0300

    feat: implement create and list logic for comments

--- apps/tasks-service/src/tasks/tasks.module.ts
+++ apps/tasks-service/src/tasks/tasks.module.ts
@@ -4,10 +4,11 @@ import { TasksService } from './tasks.service';
 import { Task } from './entities/task.entity';
 import { Comment } from './entities/comment.entity';
 import { AuditLog } from './entities/audit-log.entity';
+import { CommentsService } from './comments.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task, Comment, AuditLog])],
-  providers: [TasksService],
-  exports: [TasksService],
+  providers: [TasksService, CommentsService],
+  exports: [TasksService, CommentsService],
 })
 export class TasksModule {}
